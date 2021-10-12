fac78661d742e7a387f178cc8b19c04ff27e53cf
Marcos Ramírez Paricio <marcosramirez@gmail.com>
2021-10-12 05:24:03 +0200

    WIP

--- remembeers/js/base.js
+++ remembeers/js/base.js
@@ -1,1 +1,10 @@
+function init() {
+  refreshContent('anuncio1.html');
+}
 
+function refreshContent(content) {
+  fetch(content)
+  .then(data => data.text())
+  .then(html => document.getElementById('content').innerHTML = html);
+
+}
