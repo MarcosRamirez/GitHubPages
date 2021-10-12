fdf90057be7aacbe6003d154d27df028acc367ee
Marcos Ramírez Paricio <marcosramirez@gmail.com>
2021-10-12 05:35:17 +0200

    wip

--- remembeers/js/base.js
+++ remembeers/js/base.js
@@ -1,9 +1,17 @@
+let pages = ["anuncio1.html", "anuncio2.html"];
+let current = 0;
+
+
 function init() {
-  refreshContent('anuncio1.html');
+  setInterval(refreshContent, 30000);
 }
 
-function refreshContent(content) {
-  fetch(content)
+
+function refreshContent() {
+  if (length.pages[current]-1 >= 0) {
+    let current=0;
+  }
+  fetch(pages[current])
   .then(data => data.text())
   .then(html => document.getElementById('content').innerHTML = html);
 
